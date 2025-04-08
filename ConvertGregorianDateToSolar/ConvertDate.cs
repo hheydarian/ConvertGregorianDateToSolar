2ae8e06cd7369a470a123030f8d0af0614164b92
hheydarian <hamdhydryan12@gmail.com>
2025-04-09 00:16:13 +0330

    Add project ConvertDate

---
+++ ConvertGregorianDateToSolar/ConvertDate.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace ConvertGregorianDateToSolar;
+
+public class ConvertDate
+{
+    public string ToShamsiDate(DateTime date)
+    {
+        PersianCalendar pc = new();
+        var year = pc.GetYear(date);
+        var month = pc.GetMonth(date);
+        var day = pc.GetDayOfMonth(date);
+        return $"{year}/{month}/{day}";
+
+    }
+}
